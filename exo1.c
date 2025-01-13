a9b5119aa9cf068323433a04cb50c9a9efef8fa9
VictorVidjea <6701victor@gmail.com>
2025-01-13 10:21:08 +0100

    exo1 succès

--- exo1.c
+++ exo1.c
@@ -5,16 +5,42 @@
 #include <stdlib.h>
 
 void trois_plus_grandes_valeurs(int* tableau, int taille, int* result) {
-    
+    if (taille < 3) {
+        fprintf(stderr, "Erreur : Le tableau doit contenir au moins 3 éléments.\n");
+        exit(EXIT_FAILURE);
+    }
 
-    result[0] = -1;
-    result[1] = -1;
-    result[2] = -1;
+    int max1 = -2147483648, max2 = -2147483648, max3 = -2147483648;
+
+    for (int i = 0; i < taille; i++) {
+        if (tableau[i] > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = tableau[i];
+        } else if (tableau[i] > max2) {
+            max3 = max2;
+            max2 = tableau[i];
+        } else if (tableau[i] > max3) {
+            max3 = tableau[i];
+        }
+    }
+
+    result[0] = max1;
+    result[1] = max2;
+    result[2] = max3;
 }
 
+// Fonction pour vérifier si un nombre est premier
 int est_premier(int n) {
-    
-    return -1; // Premier
+    if (n <= 1) {
+        return 0;
+    }
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 
